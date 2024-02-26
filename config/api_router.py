fd71f54a9c00f7d42776b19cb2000e87c8128d44
windalfin <windalfin@treeclouds.com>
2024-02-27 00:57:35 +0700

    Add API endpoints for sessions, session photos, and session records

--- config/api_router.py
+++ config/api_router.py
@@ -2,11 +2,17 @@ from django.conf import settings
 from rest_framework.routers import DefaultRouter
 from rest_framework.routers import SimpleRouter
 
+from nems_proctor.proctoring.api.views import SessionPhotoViewSet
+from nems_proctor.proctoring.api.views import SessionRecordViewSet
+from nems_proctor.proctoring.api.views import SessionViewSet
 from nems_proctor.users.api.views import UserViewSet
 
 router = DefaultRouter() if settings.DEBUG else SimpleRouter()
 
 router.register("users", UserViewSet)
+router.register("sessions", SessionViewSet)
+router.register("session-photos", SessionPhotoViewSet)
+router.register("session-records", SessionRecordViewSet)
 
 
 app_name = "api"
--- nems_proctor/proctoring/api/urls.py
+++ nems_proctor/proctoring/api/urls.py
@@ -3,14 +3,9 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from .views import GetTakersByExam
-from .views import SessionPhotoViewSet
-from .views import SessionRecordViewSet
-from .views import SessionViewSet
 
 router = DefaultRouter()
-router.register(r"sessions", SessionViewSet)
-router.register(r"sessionrecords", SessionRecordViewSet)
-router.register(r"sessionphotos", SessionPhotoViewSet)
+
 
 urlpatterns = [
     path("", include(router.urls)),
