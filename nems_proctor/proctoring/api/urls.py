def7c19cde8e1a869aa7776a82833c09fcf5998d
windalfin <windalfin@treeclouds.com>
2024-02-26 15:59:20 +0700

    Add GetTakersByExam API endpoint

--- nems_proctor/proctoring/api/urls.py
+++ nems_proctor/proctoring/api/urls.py
@@ -2,6 +2,7 @@ from django.urls import include
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
+from .views import GetTakersByExam
 from .views import SessionPhotoViewSet
 from .views import SessionRecordViewSet
 from .views import SessionViewSet
@@ -13,4 +14,9 @@ router.register(r"sessionphotos", SessionPhotoViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
+    path(
+        "sessions/takers/<str:exam_code>/",
+        GetTakersByExam.as_view(),
+        name="get-takers-by-exam",
+    ),
 ]
--- nems_proctor/proctoring/api/views.py
+++ nems_proctor/proctoring/api/views.py
@@ -1,4 +1,8 @@
+from django.shortcuts import get_object_or_404
+from drf_spectacular.utils import OpenApiParameter
+from drf_spectacular.utils import OpenApiTypes
 from drf_spectacular.utils import extend_schema
+from proctoring.models import Exam
 from proctoring.models import Session
 from proctoring.models import SessionPhoto
 from proctoring.models import SessionRecord
@@ -8,6 +12,8 @@ from rest_framework.decorators import action
 from rest_framework.parsers import FormParser
 from rest_framework.parsers import MultiPartParser
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from users.api.serializers import UserSerializer
 
 from .serializers import SessionPhotoCreateSerializer
 from .serializers import SessionPhotoSerializer
@@ -16,10 +22,46 @@ from .serializers import SessionRecordSerializer
 from .serializers import SessionSerializer
 
 
+@extend_schema(
+    parameters=[
+        OpenApiParameter(
+            name="taker",
+            type=OpenApiTypes.STR,
+            location=OpenApiParameter.QUERY,
+            description="Filter sessions by taker username.",
+        ),
+        OpenApiParameter(
+            name="exam",
+            type=OpenApiTypes.STR,
+            location=OpenApiParameter.QUERY,
+            description="Filter sessions by exam code.",
+        ),
+        OpenApiParameter(
+            name="proctor",
+            type=OpenApiTypes.STR,
+            location=OpenApiParameter.QUERY,
+            description="Filter sessions by proctor username.",
+        ),
+    ],
+)
 class SessionViewSet(viewsets.ModelViewSet):
     queryset = Session.objects.all()
     serializer_class = SessionSerializer
     tags = ["Session"]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        taker_username = self.request.query_params.get("taker")
+        exam_code = self.request.query_params.get("exam")
+        proctor_username = self.request.query_params.get("proctor")
+
+        if taker_username:
+            queryset = queryset.filter(taker__username=taker_username)
+        if exam_code:
+            queryset = queryset.filter(exam__exam_code=exam_code)
+        if proctor_username:
+            queryset = queryset.filter(proctor__username=proctor_username)
+        return queryset
 
     @extend_schema(
         request=SessionPhotoCreateSerializer,
@@ -33,6 +75,14 @@ class SessionViewSet(viewsets.ModelViewSet):
     )
     def add_photo(self, request, pk=None):
         session = self.get_object()
+
+        if not session.is_active:
+            error_message = "This session has been closed and cannot accept new photo."
+            return Response(
+                {"detail": f"{error_message}"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
         serializer = SessionPhotoCreateSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save(session=session)
@@ -52,6 +102,14 @@ class SessionViewSet(viewsets.ModelViewSet):
     )
     def add_record(self, request, pk=None):
         session = self.get_object()
+
+        if not session.is_active:
+            error_message = "This session has been closed and cannot accept new record."
+            return Response(
+                {"detail": f"{error_message}"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
         serializer = SessionRecordCreateSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save(session=session)
@@ -69,3 +127,14 @@ class SessionPhotoViewSet(viewsets.ModelViewSet):
     queryset = SessionPhoto.objects.all()
     serializer_class = SessionPhotoSerializer
     tags = ["Session Photo"]
+
+
+class GetTakersByExam(APIView):
+    def get(self, request, exam_code):
+        exam = get_object_or_404(Exam, exam_code=exam_code)
+        sessions = Session.objects.filter(exam=exam).distinct("taker__id")
+        takers = [session.taker for session in sessions]
+
+        serializer = UserSerializer(takers, many=True, context={"request": request})
+
+        return Response(serializer.data)
