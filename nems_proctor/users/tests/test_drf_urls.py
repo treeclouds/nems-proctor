9cd84a4dde5d4f1c6d8ecd228d43cf5406d72198
windalfin <windalfin@treeclouds.com>
2024-02-29 01:33:17 +0700

    Update API version in user URLs

--- nems_proctor/users/tests/test_drf_urls.py
+++ nems_proctor/users/tests/test_drf_urls.py
@@ -13,10 +13,10 @@ def test_user_detail(user: User):
 
 
 def test_user_list():
-    assert reverse("api:user-list") == "/api/users/"
+    assert reverse("api:user-list") == "/api/v1/users/"
     assert resolve("/api/v1/users/").view_name == "api:user-list"
 
 
 def test_user_me():
-    assert reverse("api:user-me") == "/api/users/me/"
+    assert reverse("api:user-me") == "/api/v1/users/me/"
     assert resolve("/api/v1/users/me/").view_name == "api:user-me"
